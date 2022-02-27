231772b2876b2e8a6fde6e431067739a2065518f
EgorMazkov <mazkov99@bk.ru>
2022-02-27 15:44:42 +0300

    1

---
+++ cppModule05/ex00/Bureaucrat.hpp
@@ -0,0 +1,16 @@
+#ifndef BUREACRAT_HPP
+#define BUREACRAT_HPP
+#include <string>
+#include <iostream>
+
+
+class Bureaucrat
+{
+private:
+    std::string Name;
+public:
+    Bureaucrat();
+    ~Bureaucrat();
+};
+
+#endif
